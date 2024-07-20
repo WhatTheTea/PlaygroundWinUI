1235e572e64a444799d24ce7b1e8e343b52f6652
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-20 20:12:06 +0300

    Configure image service

---
+++ WhatTheTea.AlbumApp/Services/ImageService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel;
+using Windows.Storage.Search;
+using Windows.Storage;
+using WhatTheTea.AlbumApp.Models;
+
+namespace WhatTheTea.AlbumApp.Services
+{
+    public static class ImageService
+    {
+        public async static IAsyncEnumerable<ImageInfo> GetImagesAsync()
+        {
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation; 
+            StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync(@"Assets\Samples");
+
+            IReadOnlyList<StorageFile> imageFiles = await picturesFolder.GetFilesAsync();
+            foreach (StorageFile file in imageFiles)
+            {
+                yield return await LoadImageInfoAsync(file);
+            }
+        }
+
+        public async static Task<ImageInfo> LoadImageInfoAsync(StorageFile file)
+        {
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            ImageInfo info = new(properties,
+                                     file, file.DisplayName, file.DisplayType);
+
+            return info;
+        }
+    }
+}
---
+++ WhatTheTea.AlbumApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WhatTheTea.AlbumApp.Models;
+using WhatTheTea.AlbumApp.Services;
+
+namespace WhatTheTea.AlbumApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<ImageInfo> Images { get; } = [];
+
+        public MainViewModel()
+        {
+            Task.Run(() => ImageService.GetImagesAsync()
+                                             .ToBlockingEnumerable()
+                                             .ToList()
+                                             .ForEach(x => Images.Add(x)));
+        }
+    }
+}
