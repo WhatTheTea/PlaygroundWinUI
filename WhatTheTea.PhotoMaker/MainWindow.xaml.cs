8284ac61799c34b83c8ede9cd500e5105c068c30
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-23 14:28:53 +0300

    Update photomaker comments

--- WhatTheTea.PhotoMaker/MainWindow.xaml.cs
+++ WhatTheTea.PhotoMaker/MainWindow.xaml.cs
@@ -2,28 +2,14 @@ using CommunityToolkit.WinUI.Helpers;
 using CommunityToolkit.WinUI.UI.Controls;
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Reactive.Windows.Foundation;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
-using System.Threading.Tasks;
 
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics.Imaging;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -40,7 +26,8 @@ namespace WhatTheTea.PhotoMaker
         public MainWindow()
         {
             this.InitializeComponent();
-            // TODO: Replace camera preview with mediacapture
+            // Replace camera preview with mediacapture
+            // MediaCapture method doest not work, throws missing sink exception -_-
             CameraPreviewControl.PreviewFailed += CameraPreviewControl_PreviewFailed;
             CameraPreviewControl.StartAsync()
                 .ToObservable()
