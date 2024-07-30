a95335d5f444694be778e87c2da5cae6e15f4c7f
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-30 18:45:13 +0300

    Create market app

---
+++ WhatTheTea.MarketApp/Models.cs
@@ -0,0 +1,5 @@
+﻿namespace WhatTheTea.MarketApp.Models
+{
+    public record Product(int Id, string Title, string Description, double Price);
+    public record Seller(int Id, string Title, string Description, double Rating, Product[] Products);
+}
