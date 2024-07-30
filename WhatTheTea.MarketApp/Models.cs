c4fd3e17ff9479d6119fb83634d48940096c92d0
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-30 22:45:32 +0300

    Add logic to main viewmodel

--- WhatTheTea.MarketApp/Models.cs
+++ WhatTheTea.MarketApp/Models.cs
@@ -4,14 +4,14 @@ namespace WhatTheTea.MarketApp.Models
 {
     public class Product
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
         public required double Price { get; set; }
     }
     public class Seller
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
         public required double Rating { get; set; }
--- WhatTheTea.MarketApp/ViewModels/MainViewModel.cs
+++ WhatTheTea.MarketApp/ViewModels/MainViewModel.cs
@@ -1,13 +1,53 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
 
 using WhatTheTea.MarketApp.Data;
+using WhatTheTea.MarketApp.Models;
 
 namespace WhatTheTea.MarketApp.ViewModels
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
-        private MarketContext _marketContext = new();
+        private readonly MarketContext _marketContext = new();
 
+        public ObservableCollection<Product> Products { get; set; } = new();
 
+        public MainViewModel()
+        {
+            var dataSet = _marketContext.Products;
+
+            this.Products.Concat(dataSet.OrderBy(x => x.Title))
+                .ToList()
+                .ForEach(this.Products.Add);
+        }
+
+        [RelayCommand]
+        private void AddProduct() 
+        {
+            var product = new Product
+            {
+                Title = "Hello",
+                Description = "World",
+                Price = 1337
+            };
+            this.Products.Clear();
+            _marketContext.Products.Add(product);
+            _marketContext.SaveChanges();
+
+            this.Products.Concat(_marketContext.Products.OrderBy(x => x.Title))
+                .ToList()
+                .ForEach(this.Products.Add);
+        }
+
+        [RelayCommand]
+        private void DropDatabaseLol() 
+        {
+            _marketContext.RemoveRange(_marketContext.Products);
+        }
     }
 }
