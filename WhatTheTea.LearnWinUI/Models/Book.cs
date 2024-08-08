f55ed5971fce3c8ad547fd893e7c5cc578fd43e2
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-08-08 17:33:16 +0300

    Add Book model

---
+++ WhatTheTea.LearnWinUI/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace WhatTheTea.LearnWinUI.Models
+{
+    public enum BookType
+    {
+        Paperback,
+        Hardcover,
+        Digital
+    }
+    
+    public record Book(string Title, string Authors, BookType Type, int YearOfPublish);
+}
